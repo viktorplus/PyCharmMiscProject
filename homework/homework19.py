9459762d653680d1d441fbcdab1a9f557733c527
Viktorplus <36795799+viktorplus@users.noreply.github.com>
2026-01-11 03:28:40 +0100

    update homework19.py

--- homework/homework19.py
+++ homework/homework19.py
@@ -35,7 +35,7 @@
 # # Пример вывода:
 # # {'anna': {'a': 2, 'n': 2}, 'bennet': {'b': 1, 'e': 2, 'n': 2, 't': 1}, 'john': {'j': 1, 'o': 1, 'h': 1, 'n': 1}}
 #
-# result = {name : {ch:name.count(ch) for ch in name} for name in set(words)}
+# result = {name : {ch:name.count(ch) for ch in set(name)} for name in words}
 # print(result)
 #
 # # вариант из summary
@@ -56,59 +56,59 @@
 #
 # Создайте словарь с ключами-группами и словарями со студентами в качестве значений.
 # Данные:
-students = {"Аня": 92, "Боря": 76, "Ваня": 65, "Галя": 48, "Дима": 88, "Ева": 54}
-groups = ["Отличники", "Хорошисты", "Троечники", "Не сдали"]
-# Пример вывода:
-# Распределение по группам:
-# {'Отличники': {'Аня': 92, 'Дима': 88}, 'Хорошисты': {'Боря': 76}, 'Троечники': {'Ваня': 65, 'Ева': 54}, 'Не сдали': {'Галя': 48}}
-
-result = {group: {} for group in groups}
-for key, value in students.items():
-    if value >= 85:
-        result["Отличники"][key]= value
-    elif value >= 70:
-        result["Хорошисты"][key]= value
-    elif value >= 50:
-        result["Троечники"][key]= value
-    else:
-        result["Не сдали"][key]= value
-print("Распределение по группам:")
-print(result)
-
-# вариант 2
-rules = [
-    ("Отличники", 85, 100),
-    ("Хорошисты", 70, 84),
-    ("Троечники", 50, 69),
-    ("Не сдали",  0, 49),
-]
-
-result = {group: {} for group, min, max in rules}
-
-for name, score in students.items():
-    for group, min, max in rules:
-        if min <= score <= max:
-            result[group][name] = score
-            break
-print("Распределение по группам:")
-print(result)
-
-# вариант из summary
-students = {"Аня": 92, "Боря": 76, "Ваня": 65, "Галя": 48, "Дима": 88, "Ева": 54}
-groups = ["Отличники", "Хорошисты", "Троечники", "Не сдали"]
-
-student_groups = {group: {} for group in groups}
-
-for name, score in students.items():
-    if score >= 85:
-        group_name = "Отличники"
-    elif 70 <= score < 85:
-        group_name = "Хорошисты"
-    elif 50 <= score < 70:
-        group_name = "Троечники"
-    else:
-        group_name = "Не сдали"
-    student_groups[group_name][name] = score
-
-print("Распределение по группам:")
-print(student_groups)
+# students = {"Аня": 92, "Боря": 76, "Ваня": 65, "Галя": 48, "Дима": 88, "Ева": 54}
+# groups = ["Отличники", "Хорошисты", "Троечники", "Не сдали"]
+# # Пример вывода:
+# # Распределение по группам:
+# # {'Отличники': {'Аня': 92, 'Дима': 88}, 'Хорошисты': {'Боря': 76}, 'Троечники': {'Ваня': 65, 'Ева': 54}, 'Не сдали': {'Галя': 48}}
+#
+# result = {group: {} for group in groups}
+# for key, value in students.items():
+#     if value >= 85:
+#         result["Отличники"][key]= value
+#     elif value >= 70:
+#         result["Хорошисты"][key]= value
+#     elif value >= 50:
+#         result["Троечники"][key]= value
+#     else:
+#         result["Не сдали"][key]= value
+# print("Распределение по группам:")
+# print(result)
+#
+# # вариант 2
+# rules = [
+#     ("Отличники", 85, 100),
+#     ("Хорошисты", 70, 84),
+#     ("Троечники", 50, 69),
+#     ("Не сдали",  0, 49),
+# ]
+#
+# result = {group: {} for group, min, max in rules}
+#
+# for name, score in students.items():
+#     for group, min, max in rules:
+#         if min <= score <= max:
+#             result[group][name] = score
+#             break
+# print("Распределение по группам:")
+# print(result)
+#
+# # вариант из summary
+# students = {"Аня": 92, "Боря": 76, "Ваня": 65, "Галя": 48, "Дима": 88, "Ева": 54}
+# groups = ["Отличники", "Хорошисты", "Троечники", "Не сдали"]
+#
+# student_groups = {group: {} for group in groups}
+#
+# for name, score in students.items():
+#     if score >= 85:
+#         group_name = "Отличники"
+#     elif 70 <= score < 85:
+#         group_name = "Хорошисты"
+#     elif 50 <= score < 70:
+#         group_name = "Троечники"
+#     else:
+#         group_name = "Не сдали"
+#     student_groups[group_name][name] = score
+#
+# print("Распределение по группам:")
+# print(student_groups)
